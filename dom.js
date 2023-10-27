091feed769bbde591b91525fb5c9ee2cf4f88143
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-10-27 08:39:18 -0400

    klnl

--- dom.js
+++ dom.js
@@ -1,34 +1,30 @@
-/* const form = document.querySelector("#form");
-console.log(form);
-const firstName = document.querySelector(".first-name");
-DOMSelectors = {
-    form: document.querySelector
-}
-
-DOMSelectors.form.addEventListener("submit", function () {
-    console.log(DOMSelectors)
-    event.preventDefault();
-});
- */
-// foods
-
 const DOMSelectors = {
-         btn:document.getElementById("btn"),
-         input:document.querySelector(`#input`),
-       box:document.getElementById("container"),
+    btn:document.getElementById("btn"),
+    input:document.querySelector(`#input`),
+    box:document.getElementById("container"),
+
 }
+
 DOMSelectors.btn.addEventListener("click", function () {
     let task = DOMSelectors.input.value;
     DOMSelectors.box.insertAdjacentHTML("beforeend", ` <div><p id="card"> ${task}.
      <button class="btnd">-</button> </p> </div> `)
 erase();
-    erase();
+});
+
+const tasks = {
+btnd: btnd  = "#btnd",
+card: card = "#card"
 }
 
+DOMSelectors.btn.addEventListener("click", function () {
+    DOMSelectors.input.value = "";
+});
+
+function erase() {
 const remove = document.querySelectorAll(".btnd");
 remove.forEach((buttond) => {
     buttond.addEventListener("click", (event) => {
         event.target.parentElement.remove();
-        backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-    });
-})
+});
+})};
