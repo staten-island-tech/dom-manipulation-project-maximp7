8b50e119fdbb885d5eb903347fed3502424486f0
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-11-07 22:58:47 -0500

    sdds

--- dom.js
+++ dom.js
@@ -1,33 +1,46 @@
 const DOMSelectors = {
+  card_holder: document.getElementById("card_holder"),
+  card_form: document.getElementById("card_form"),
+  name_input: document.getElementById("name_input"),
+  color_input: document.getElementById("color_input"),
+  taste_input: document.getElementById("taste_input"),
   btn: document.getElementById("btn"),
-  input: document.querySelector(`#input`),
-  box: document.getElementById("container"),
 };
 
-DOMSelectors.btn.addEventListener("click", function () {
-  let task = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
-    ` <div><p id="card"> ${task}.
-     <button class="btnd">-</button> </p> </div> `
-  );
-  erase();
+DOMSelectors.card_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const new_card = {
+    name: DOMSelectors.name_input.value,
+    color: DOMSelectors.color_input_value,
+    taste: DOMSelectors.taste_input_value,
+  };
+  add_card(new_card);
+  clear_fields();
 });
 
-const tasks = {
-  btnd: (btnd = "#btnd"),
-  card: (card = "#card"),
-};
+function add_card(card) {
+  DOMSelectors.card_holder.insertAdjacentHIML(
+    "afterbegin",
+    `<div class="card">
+        <h1 class="card-title">${card.name}</h1>
+        <h2 class="age">${card.color}</h2> 
+        <img src="${card.taste}" alt="">
+        <button class="remove">Remove</button>
+        </div>`
+  );
+
+  const button = document.querySelector(".remove");
+  button.addEventListener("click", (event) => {
+    button.parentElement.remove();
+  });
+}
 
 DOMSelectors.btn.addEventListener("click", function () {
   DOMSelectors.input.value = "";
 });
 
-function erase() {
-  const remove = document.querySelectorAll(".btnd");
-  remove.forEach((buttond) => {
-    buttond.addEventListener("click", (event) => {
-      event.target.parentElement.remove();
-    });
-  });
+function clear_fields() {
+  DOMSelectors.name_input.value = null;
+  DOMSelectors.color_input.value = null;
+  DOMSelectors.taste_input.value = null;
 }
