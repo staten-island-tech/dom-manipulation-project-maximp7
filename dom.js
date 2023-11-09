6cde706b937039d26a5bdde60b472f1499169f1d
maximp7 <144476708+maximp7@users.noreply.github.com>
2023-11-08 20:13:49 -0500

    pojp

--- dom.js
+++ dom.js
@@ -1,45 +1,46 @@
-const DOMSelectors = {
-  card_holder: document.getElementById("card_holder"),
-  card_form: document.getElementById("card_form"),
-  name_input: document.getElementById("name_input"),
-  color_input: document.getElementById("color_input"),
-  taste_input: document.getElementById("taste_input"),
-  btn: document.getElementById("btn"),
+const DOMselectors = {
+  form: document.getElementById("form"),
+  FruitNameform: document.getElementById("fruit-name"),
+  FruitImgform: document.getElementById("image"),
+  FruitDescform: document.getElementById("Color"),
+  title: document.querySelectorAll("card-title"),
+  image: document.querySelectorAll("pic"),
+  desc: document.querySelectorAll(".fruit-desc"),
+  button: document.getElementById("btn"),
 };
 
-DOMSelectors.card_form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const new_card = {
-    name: DOMSelectors.name_input.value,
-    color: DOMSelectors.color_input_value,
-    taste: DOMSelectors.taste_input_value,
+function makeFruit() {
+  const make = {
+    name: DOMselectors.FruitNameform.value,
+    img: DOMselectors.FruitImgform.value,
+    desc: DOMselectors.FruitDescform.value,
   };
-  add_card(new_card)
-  clear_fields()
-});
-
-function add_card(card) {
-  DOMSelectors.card_holder.insertAdjacentHIML(
-    "afterbegin",
-    `<div class="card">
-        <h1 class="card-title">${card.name}</h1>
-        <h2 class="age">${card.color}</h2> 
-        <img src="${card.taste}" alt="">
-        <button class="remove">Remove</button>
-        </div>`
-  );
-
-  const button = document.querySelector(".remove")
-  button.addEventListener("click", (event) => {
-    button.parentElement.remove();
-  });
+  return make;
 }
 
-DOMSelectors.btn.addEventListener("click", function () {
-  DOMSelectors.input.value = "";
+function addcard(Animal) {
+  document.querySelector(".flex-container").insertAdjacentHTML("afterbegin", `div class="card">
+  <h1 class="card-title">${Fruit.name}</h1>
+  <img src="${Fruit, img}" alt="" class-"pic">
+  <h2 class="animal-desc">${Animal.desc}</h2>
+  <button class="btnd">Remove the Card</button>
+  </div>`);
+}
+
+const btnr = document.querySelector(".btnd");
+btnr.addEventListener("click", (event) => {
+  btnr.parentElement.remove(Fruit)
+})
+
+
+DoMselectors.form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const Concard = makeFruit();
+  addcard(Concard);
+  emptytbox();
 });
 
-function clear_fields() {
+function emptytbox() {
   DOMSelectors.name_input.value = null;
   DOMSelectors.color_input.value = null;
   DOMSelectors.taste_input.value = null;
